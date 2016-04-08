3728aa2a0d34e335b65456be6510e66d388f3757
SleepingWhale <vladimir@Vladimirs-MacBook-Air.local>
2016-04-08 10:36:05 +0300

    fixing issue
    game is not over when no possible moves left.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-(function(){
+//(function(){
 // game board constructor
 function Board(side) {
   this.side = side;
@@ -232,7 +232,7 @@ PlayRoom.prototype.switchPlayers = function() {
   this.currentPlayer.panelElement.className = 'panel panel-default';
   this.otherPlayer = [this.currentPlayer, this.currentPlayer = this.otherPlayer][0];
   this.currentPlayer.panelElement.className = 'panel panel-primary';
-  if(this.currentPlayer.color){
+  if (this.currentPlayer.color) {
     this.boardBGElement.className = this.boardBGElement.className.replace(' up', ' down');
   } else {
     this.boardBGElement.className = this.boardBGElement.className.replace(' down', ' up');
@@ -374,7 +374,7 @@ function Game() {
 
 Game.prototype.init = function(el) {
   var i, j, o, figureBlack, figureWhite,
-    figureRows = 3;
+    figureRows = 1;
   this.board = new Board(8);
   this.board.drawBoard(el);
   this.board.boardElement.addEventListener('click', this.move.bind(this), false);
@@ -425,7 +425,17 @@ Game.prototype.isKing = function(figure) {
 };
 
 Game.prototype.isWinner = function() {
-  if (Object.getOwnPropertyNames(this.possibleMoves).length === 0) {
+  var gameOver = true,
+    move;
+  if (Object.getOwnPropertyNames(this.possibleMoves).length > 0) {
+    for (move in this.possibleMoves) {
+      if (this.possibleMoves[move]) {
+        gameOver = false;
+      }
+    }
+  }
+
+  if (gameOver) {
     room.setTotalScore(true);
     room.stopStopwatch.call(room);
     room.showMessage('У нас есть победитель: "' + room.otherPlayer.name + '". Нажать здесь для начала новой партии.', room.gameReloading);
@@ -590,4 +600,4 @@ Game.prototype.move = function(e) {
 var game;
 var room = new PlayRoom();
 room.start();
-})();
+//})();
